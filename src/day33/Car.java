0b146d2afd847f586b5d375c234ef5b03c194ca8
Serkan_PC <serdar2225@gmail.com>
2019-11-17 15:56:23 -0500

    I have added some additional tests

---
+++ src/day33/Car.java
@@ -0,0 +1,12 @@
+package day33;
+
+public class Car {
+    int windows;
+    int wheels;
+    String brand;
+    int year;
+    String countryOfOrigin;
+    public static void main(String[] args) {
+
+    }
+}
--- src/day33/TestingSettings.java
+++
@@ -1,7 +0,0 @@
-package day33;
-
-public class TestingSettings {
-    public static void main(String[] args) {
-        System.out.println("Some cool tests");
-    }
-}
