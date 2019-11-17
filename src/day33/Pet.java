32d7bfadf300e911971eecb8e70a056be1eecb80
Serkan_PC <serdar2225@gmail.com>
2019-11-17 15:58:11 -0500

    I have added some additional tests

---
+++ src/day33/Pet.java
@@ -0,0 +1,13 @@
+package day33;
+
+public class Pet {
+    String name;
+    int age;
+    int numberOfLegs;
+    String breed;
+    boolean cute;
+
+    public static void main(String[] args) {
+
+    }
+}
