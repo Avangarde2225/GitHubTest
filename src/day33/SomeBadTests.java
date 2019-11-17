4ce6c156507c3be3fdd33a8776cf92c646699215
Serkan_PC <serdar2225@gmail.com>
2019-11-17 14:41:27 -0500

    I have added some additional tests

--- src/day33/SomeBadTests.java
+++
@@ -1,7 +0,0 @@
-package day33;
-
-public class SomeBadTests {
-    public static void main(String[] args) {
-        // I have not done any test yet
-    }
-}
---
+++ src/day33/TestingSecurity.java
@@ -0,0 +1,7 @@
+package day33;
+
+public class TestingSecurity {
+    public static void main(String[] args) {
+        System.out.println("Security is being checked");
+    }
+}
