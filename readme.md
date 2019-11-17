401027bb0bb3ac5f01f436634a031cd5ce18bd98
Serkan_PC <serdar2225@gmail.com>
2019-11-17 14:22:59 -0500

    I have added some additional tests

--- readme.md
+++ readme.md
@@ -4,4 +4,4 @@ as part of the coursework
 * item 1
 * item 2
 
-[google.com] (google.com)
+[google.com] (http:google.com)
---
+++ src/day33/TestingSettings.java
@@ -0,0 +1,7 @@
+package day33;
+
+public class TestingSettings {
+    public static void main(String[] args) {
+        System.out.println("Some cool tests");
+    }
+}
